2e0523152bd0770ddf3a104197653619de85a6fa
unknown <mansikeshari1999@gmail.com>
2021-06-19 21:01:04 +0530

    completed 8th THA

--- THA7 and 8/index.js
+++ THA7 and 8/index.js
@@ -12,12 +12,58 @@ console.log(obj);
 var v=Object.keys(obj).length;//Object.keys(obj) returns array of keys and length returns length of array
 console.log(v);
 //que4
-function Object()
-{
-    this.vol=function(r,h){
-      return(2*3.14*r*h);
-    }
+function display(){
+const library=[
+  {
+    author:'Bill Gates',
+    title:'The Road Ahead',
+    readingStatus:true
+  },
+ {
+  author:'Steve Jobs',
+  title:'Walter Isaacson',
+  readingStatus:true
+ },
+ {
+  author:'Suzanne Collins',
+  title:'Mockingjay',
+  readingStatus:false
+ }
+];
+ for(let i=0;i<library.length;i++)
+ {
+   console.log(library[i].author);
+   console.log(library[i].title);
+   console.log(library[i].readingStatus);
+ }
 }
-const volume=Object.vol(2,3);
-console.log(volume);
+display();
+ //que 5
+const cylinder={radius:5,height:6};
+const vol=2*3.14*cylinder.radius*cylinder.height;
+console.log(vol);
+//que 6
+var library=[
+  {
+    author:'Bill Gates',
+    title:'The Road Ahead',
+    libraryID:1254
+  },
+ {
+  author:'Steve Jobs',
+  title:'Walter Isaacson',
+  libraryID:4264
+ },
+ {
+  author:'Suzanne Collins',
+  title:'Mockingjay',
+  libraryID:3245
+ }
+];
+library.sort((a, b) => {
+ return b.libraryID-a.libraryID;
+});
+library.forEach((item)=>{
+  console.log(item);
+});
 
