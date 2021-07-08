ed43e9231d097411612f634ba9bd6a13a74ff042
unknown <mansikeshari1999@gmail.com>
2021-07-08 22:25:14 +0530

    ChessBoard added

--- card/src/Card.jsx
+++ card/src/Card.jsx
@@ -4,7 +4,7 @@ const Card=()=>{
     return(
         <div className="Card">
             <div className="img">
-              {/* <img src="./img/tom.png" alt=""></img> */}
+              {/* <img src={"./img/tom.png"} alt=""></img> */}
               <img src="https://purepng.com/public/uploads/large/purepng.com-tom-and-jerry-happytom-and-jerrytomjerryanimated-seriesin-1940characters-1701528659229lrdcn.png" alt=""></img>
             </div>
             <h2>Hello!! This is a card</h2>
