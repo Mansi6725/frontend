f6338184ad76eaa08ffa88f524841c20fd8d21cd
unknown <mansikeshari1999@gmail.com>
2021-07-11 00:33:35 +0530

    completed THA19

---
+++ tha19counter/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Comp1 from "./Comp1";
+import Comp2 from "./Comp2";
+import Comp3 from "./Comp3";
+import Comp4 from "./Comp4";
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Comp1></Comp1>
+        <Comp2></Comp2>
+        <Comp3></Comp3>
+        <Comp4></Comp4>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tha19counter/src/Comp4.jsx
@@ -0,0 +1,11 @@
+import {useState} from "react";
+import "./Comp.css";
+function Comp4(){
+ const [state,setState]=useState(0);
+ return(
+  <div className="comp" onClick={()=>setState(state+1)}>
+      <h1>{state}</h1>
+  </div>
+ );
+}
+export default Comp4;
